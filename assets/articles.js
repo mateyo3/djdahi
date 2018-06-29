275c5dc37a4720efe786e5916bc276c8004c321a
mateyo3 <mateoreyes720@gmail.com>
2018-06-29 16:46:40 -0700

    initial commit and article.js data

---
+++ assets/articles.js
@@ -0,0 +1,158 @@
+var articles = [
+  {
+    site: "Rollingstone.com",
+    title: "DJ Dahi: Meet the Master Beatmaker for Drake, Dr. Dre, Kendrick Lamar (2015)",
+    articleURL: "https://www.rollingstone.com/music/news/dj-dahi-meet-the-master-beatmaker-for-drake-dr-dre-kendrick-lamar-20150923",
+    imgURL: "https://img.wennermedia.com/article-leads-horizontal-800/rs-210636-IMG_3337.jpg",
+    date: "date",
+    author: "author",
+    contentSample:"content",
+    devNotes:"notes"
+  },
+  {
+    site: "Spin.com",
+    title: "Producer DJ Dahi Talks the 5 Songs He Made For Kendrick Lamar’s DAMN. (2017)",
+    articleURL: "https://www.spin.com/2017/04/dj-dahi-interview-damn-kendrick-lamar/",
+    imgURL: "https://static.spin.com/files/2017/04/13438797_1118783008160620_421842127413038052_n-1492993913-640x427.jpg",
+    date: " ",
+    author: " ",
+    devNotes: " "
+  },
+  {
+    site: "npr.org",
+    title: "DJ Dahi: Making The Move From Bass Line To Top Billing (2016)",
+    articleURL: "https://www.npr.org/sections/therecord/2016/02/23/466130812/dj-dahi-making-the-move-from-bass-line-to-top-billing",
+    imgURL: "https://media.npr.org/assets/img/2016/02/09/dacoury-20144625_wide-f8b429ce2ebcd66c399b635791e6740c512326c5-s700-c85.jpg",
+    date: " ",
+    author: " ",
+    devNotes: " "
+  },
+  {
+    site: "laweekly.com",
+    title: "DJ Dahi Was a College Dorm Adviser When His First Kendrick Beat Blew Up (2017)",
+    articleURL: "http://www.laweekly.com/music/dj-dahis-first-kendrick-lamar-beat-money-trees-made-him-an-in-demand-hip-hop-producer-8174720",
+    imgURL: "https://images1.laweekly.com/imager/u/745xauto/8174724/dj-dahi-closeup-keyboards-danny-liao.jpg",
+    imgURL2: "https://images1.laweekly.com/imager/u/745xauto/8174725/dj-dahi-studio-bw-danny-liao.jpg",
+    date: " ",
+    author: " ",
+    devNotes: " "
+  },
+  {
+    site: "potholesinmyblog.com",
+    title: "DJ Dahi Discusses Producing Drake’s "Worst Behavior", His Influences And Music Process",
+    articleURL: "http://potholesinmyblog.com/watch-dj-dahi-discusses-producing-drakes-worst-behavior-his-influences-and-music-process/",
+    imgURL: "http://potholesinmyblog.flywheelsites.com/wp-content/uploads/2014/11/dj-dahi.jpg",
+    date: " ",
+    author: " ",
+    devNotes: " "
+  },
+  {
+    site: "mccartenvoilins.com",
+    title: "From DJ to Hit",
+    articleURL: "http://mccartenviolins.com/2016/02/from-dj-to-hit/",
+    imgURL: "http://mccartenviolins.com/wp-content/uploads/2016/02/tumblr_n1yxupH1Ap1rqxid0o1_500-460x333.jpg",
+    date: " ",
+    author: " ",
+    devNotes: " "
+  },
+  {
+    site:"redbull.com",
+    title: "Track Premiere: Race Banyon's "Surface"",
+    articleURL: "https://www.redbull.com/nz-en/track-premiere-race-banyons-surface",
+    imgURL: "https://image.redbull.com/rbcom/010/2016-02-25/1331778937379_2/0010/1/800/533/1/race-banyon-and-dj-dahi.jpg",
+    date: "2016",
+    author: "Red Bull Sound Select",
+    devNotes: " "
+  },
+  {
+    site: "zimbio.com",
+    title: "2016 SESAC Pop Music Awards - Show",
+    articleURL: "http://www.zimbio.com/photos/DJ+Dahi/2016+SESAC+Pop+Music+Awards+Show/_66IPXXImqk",
+    imgURL: "http://www2.pictures.zimbio.com/gi/DJ+Dahi+2016+SESAC+Pop+Music+Awards+Show+_66IPXXImqkl.jpg",
+    date: "2016",
+    author: " ",
+    devNotes: "photo only: (L-R) Greg Johnson, James Leach, DJ Dahi, Keith Darcy and Rich Christina pose with an award onstage during the 2016 SESAC Pop Music Awards on April 18, 2016 in New York City."
+  },
+  {
+    site: "madannarama.com",
+    title: "Madonna confirms new collaboration with DJ Dahi & Blood Diamonds",
+    articleURL: "http://www.madonnarama.com/posts-en/2014/09/20/madonna-confirms-new-collaboration-with-dj-dahi-blood-diamonds/",
+    imgURL: "http://www.madonnarama.com/artworks/posts/en-27651v02.jpg",
+    date: " ",
+    author: " ",
+    devNotes: " "
+  },
+  {
+    site: "okayplayer.com",
+    title: "DJ Dahi Showcases His Musicianship On 'Rhythm Roulette'",
+    articleURL: "http://www.okayplayer.com/news/dj-dahi-rhythm-roulette-video.html",
+    imgURL: "http://okp-cdn.okayplayer.com/wp-content/uploads/2016/06/dj-dahi-rhythm-roulette-video.jpg",
+    date: " ",
+    author: " ",
+    devNotes: "article directs users to Mass Appeal"
+  },
+  {
+    site: "travellingprogrammer.wordpress.com",
+    title: "PLAYLIST: DJ DAHI",
+    articleURL: "https://travellingprogrammer.wordpress.com/2013/01/19/dj-dahi/",
+    imgURL: "https://travellingprogrammer.files.wordpress.com/2013/01/dahi.jpg?w:470&h:248",
+    date: "2019",
+    author: " ",
+    devNotes: " "
+  },
+  {
+    site: "yourinfodaily.com",
+    title: "Dj Dahi discusses music in this short documentary ",
+    articleURL: "http://www.yourinfodaily.com/performances-interviews-documentaries/dj-dahi-discusses-music-in-this-short-documentary",
+    imgURL: "http://www.yourinfodaily.com/uploads/2/6/5/6/26565410/6612133_orig.jpg",
+    date: "2014",
+    author: " ",
+    devNotes: "not really an article"
+  },
+  {
+    site: "imperiousent.com",
+    title: "dj-dahi-coors-light",
+    articleURL: "http://imperiousent.com/big-boi-ft-adam-levine-mic-jack-review/dj-dahi-coors-light/",
+    imgURL: "http://imperiousent.com/wp-content/uploads/2017/04/dj-dahi-coors-light.jpg",
+    date: "2017",
+    author: " ",
+    devNotes: " "
+  },
+  {
+    site: "thefader.com",
+    title: "Beat Construction: DJ Dahi",
+    articleURL: "http://www.thefader.com/2013/10/31/beat-construction-dj-dahi",
+    imgURL: "http://thefader-res.cloudinary.com/images/w_1440,c_limit,f_auto,q_auto:eco/fofdpwr87oz8rga9k4vs/beat-construction-dj-dahi.jpg",
+    date: "2013",
+    author: "NAOMI ZEICHNER",
+    devNotes: " "
+  },
+  {
+    site: "djbooth.net",
+    title: "Meet DJ Dahi, Your Favorite Rapper's Favorite Producer",
+    articleURL: "https://djbooth.net/features/2015-08-10-meet-dj-dahi-your-favorite-rappers-favorite-producer",
+    imgURL: "https://djbooth.net/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_400/MTUzNDg2MDE0Mjk4OTI0MjMw/dj-dahi-profilejpg.webp",
+    imgURL2: "https://djbooth.net/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_400/MTUzNDg2MDE0Mjk2MDQwNjQ2/dj-dahijpg.webp",
+    date: "2015",
+    author: "Kevin Taylor",
+    devNotes: " "
+  },
+  {
+    site: "thesource.com",
+    title: "J. COLE & DJ DAHI IN THE STUDIO",
+    articleURL: "http://thesource.com/2014/03/03/j-cole-dj-dahi-in-the-studio/",
+    imgURL: "http://thesource.com/wp-content/uploads/2014/03/dahi-j.-cole.png",
+    date: "2014",
+    author: "Khari",
+    devNotes: " "
+  },
+  {
+    site: "audibletreats.com",
+    title: "Video: BTS of DJ Dahi on Producing Music That Transcends Genres",
+    articleURL: "http://www.audibletreats.com/dj-dahi_pr3/",
+    imgURL: "http://www.audibletreats.com/wp-content/uploads/2015/08/reFRESHing_Sound_with_DJ_Dahi-3.gif" ,
+    date: "2015",
+    author: " ",
+    devNotes: "image is a gif"
+  },
+];
