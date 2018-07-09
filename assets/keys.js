4315fd3559535a248a57e3e9b8e5b65a74d682b5
mateyo3 <mateoreyes720@gmail.com>
2018-07-09 15:58:02 -0700

    twitter

--- assets/keys.js
+++ assets/keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'ahm6iI4GenBBh9gS1IRpHagIN',
+  consumer_secret: 'NpKeTPf4GpxxhM22ATMT9Cjk0bYMwogNp5iNlmTUCihIryykMq',
+  access_token_key: '	922996164172984320-oG81YotcztUV7MC7Lv5XpR1uq0tQBzq',
+  access_token_secret: 'p31HwVTMonBEXzwRCe85QZDi5pNrAqzUobcddLUGBj4bK',
 }
 
 module.exports = twitterKeys;
